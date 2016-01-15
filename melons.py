a3f09ffed69a0e1120d6424906dc24f87795f35b
Hackbright Student <info@hackbrightacademy.com>
2016-01-15 12:35:28 -0800

    updated InternationalMelonOrder, updated get_total, code reviewed

--- melons.py
+++ melons.py
@@ -14,8 +14,17 @@ class AbstractMelonOrder(object):
         """Calculate price."""
 
         base_price = 5
+        
+        if self.species == "Christmas melon":
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
+        
+        if self.order_type == "international" and self.qty < 10:
+            total += 3
+
         return total
+
 
     def mark_shipped(self):
         """Set shipped to true."""
@@ -26,7 +35,6 @@ class AbstractMelonOrder(object):
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
-
 
     def __init__(self, species, qty):
         """Initialize melon order attributes"""
@@ -50,10 +58,10 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Initialize melon order attributes"""
         # self.species = species
         # self.qty = qty
-        self.order_type = "international"
+        #self.order_type = "international"
         self.country_code = country_code
-        self.tax = 0.17
-        super(InternationalMelonOrder, self).__init__(species, qty)
+        #self.tax = 0.17
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
 
 
     def get_country_code(self):
